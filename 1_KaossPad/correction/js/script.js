893a6a73dd1979f133c1ffa5ad06ee3ce37bf66c
Emilie Zawadzki <emilie.zawadzki@ircam.fr>
2018-12-03 11:33:55 +0100

    [KaossPad] : improve

--- 1_KaossPad/correction/js/script.js
+++ 1_KaossPad/correction/js/script.js
@@ -1,51 +1,48 @@
-let freqMax = 20000 // frequency max
-let halfPage // half page in pixel
+// declare frequency max
+let freqMax = 20000
 
 // use microphone as audio input
 let voice = new Pizzicato.Sound({ source: 'input' })
 
 // declare ping pong delay
 let pingPongDelay = new Pizzicato.Effects.PingPongDelay({
-  feedback: 0.6,
-  time: 0.4,
-  mix: 0.5
+    feedback: 0.6,
+    time: 0.4,
+    mix: 0.5
 })
 
 // declare low pass filter
 let lowPassFilter = new Pizzicato.Effects.LowPassFilter({
-  frequency: 400,
-  peak: 10
+    frequency: 200,
+    peak: 10
 })
 
 // declare high pass filter
 let highPassFilter = new Pizzicato.Effects.HighPassFilter({
-  frequency: 10,
-  peak: 10
-})
+    frequency: 10,
+    peak: 10
+});
 
 // add effect to microphone source
-voice.addEffect(pingPongDelay)
-voice.addEffect(lowPassFilter)
-voice.addEffect(highPassFilter)
+voice.addEffect(pingPongDelay);
+voice.addEffect(lowPassFilter);
+voice.addEffect(highPassFilter);
 
-window.addEventListener('load', function () {
-  // define half page
-  halfPage = document.body.clientHeight / 2
-  // play
-  document.getElementById('play-button').addEventListener('click', function (event) {
-    voice.play()
-  }, false)
-  // stop
-  document.getElementById('stop-button').addEventListener('click', function (event) {
-    voice.stop()
-  }, false)
-  // on mouse move, vary effects parameters functions of X / Y mouse position
-  document.body.addEventListener('mousemove', function (event) {
-    // varying ping pong delay  
-    pingPongDelay.feedback = event.pageX / document.body.clientWidth
-    // varying low pass filter 
-    lowPassFilter.frequency = event.pageY / document.body.clientHeight * freqMax
-    // varying high pass filter  
-    highPassFilter.frequency = event.pageY / document.body.clientHeight * freqMax
-  }, false)
-})
+
+// play event listener
+document.getElementById('play-button').addEventListener('click', function (event) {
+    voice.play();
+}, false);
+// stop event listener
+document.getElementById('stop-button').addEventListener('click', function (event) {
+    voice.stop();
+}, false);
+// on mouse move, vary effects parameters functions of X / Y mouse position
+document.body.addEventListener('mousemove', function (event) {
+    // vary ping pong delay
+    pingPongDelay.feedback = event.pageX / document.body.clientWidth;
+    // vary low pass filter 
+    lowPassFilter.frequency = event.pageY / document.body.clientHeight * freqMax;
+    // vary high pass filter    
+    highPassFilter.frequency = event.pageY / document.body.clientHeight * freqMax;
+}, false);
--- 1_KaossPad/exo/js/script.js
+++ 1_KaossPad/exo/js/script.js
@@ -1,1 +1,23 @@
-// fill it !
+// declare frequency max
+
+// use microphone as audio input
+
+// declare ping pong delay
+
+// declare low pass filter
+
+// declare high pass filter
+
+// add effect to microphone source
+
+// play event listener
+
+// stop event listener
+
+// on mouse move, vary effects parameters functions of X / Y mouse position
+    
+    // vary ping pong delay on X axis
+
+    // vary low pass filter on Y axis 
+
+    // vary high pass filter on Y axis
